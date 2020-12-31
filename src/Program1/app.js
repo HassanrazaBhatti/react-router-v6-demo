edf7e70fc2d4a9e5dcfe41829085f6ed9a11e411
Hassanbhatti <66300348+HassanrazaBhatti@users.noreply.github.com>
2021-01-01 00:10:22 +0500

    Create app.js
    Program 1 done

---
+++ src/Program1/app.js
@@ -0,0 +1,6 @@
+// Question No 1
+
+
+var C = prompt("Enter temprature in calsius");
+const updated_tempratuer = 9/5 * C + 32;
+alert("The converted temprature from calsius is   " + updated_tempratuer);
